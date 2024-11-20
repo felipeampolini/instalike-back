993bc768508d5a4bf94f34d402b591fa873efc48
Felipe <felipeampolini@hotmail.com>
2024-11-20 19:57:48 -0300

    Aula 3 imersao back-end alura: Conexao db, separação de arquivos em pastas

--- server.js
+++ server.js
@@ -1,57 +1,11 @@
 import express from "express";
-
-const posts = [
-    {
-        id: 1,
-        descricao: "Foto teste",
-        imagem: "https://placecats.com/millie/300/250"
-    },
-    {
-        id: 2,
-        descricao: "Paisagem deslumbrante",
-        imagem: "https://placecats.com/millie/300/2506"
-    },
-    {
-        id: 3,
-        descricao: "Cachorro fofo",
-        imagem: "https://placecats.com/millie/300/2506"
-    },
-    {
-        id: 4,
-        descricao: "Comida deliciosa",
-        imagem: "https://placecats.com/millie/300/2506"
-    },
-    {
-        id: 5,
-        descricao: "Arte abstrata",
-        imagem: "https://placecats.com/millie/300/2506"
-    },
-    {
-        id: 6,
-        descricao: "Gato curioso",
-        imagem: "https://placecats.com/millie/300/2506"
-    }
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const porta = 3000;
 const app = express();
-app.use(express.json());
+
+routes(app);
 
 app.listen(porta, () => {
     console.log("Servidor aberto em http://localhost:"+porta);
-});
-
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
-
-function buscarPostPorId(id){
-    return posts.findIndex((post) => {
-        return post.id === Number(id);
-    });
-}
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorId(req.params.id);
-    res.status(200).json(posts[index]);
 });
